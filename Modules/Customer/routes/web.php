a77fc717e00e4f4d9ad766ddf83605bd0711dfd7
vw-uday <verloop.dev4@gmail.com>
2026-08-13 16:53:34 +0530

    invoice error solve

--- Modules/Customer/routes/web.php
+++ Modules/Customer/routes/web.php
@@ -4,15 +4,15 @@ use Illuminate\Support\Facades\Route;
 use Modules\Customer\App\Http\Controllers\CustomerController;
 
 
-// Route::group([
-//     'prefix' => '',
-//     'as' => 'manage.',
-//     'middleware' => ['auth','PreventBackHistory']
-// ], function () {
+Route::group([
+    'prefix' => '',
+    'as' => 'manage.',
+    'middleware' => ['auth','PreventBackHistory']
+], function () {
 //     Route::get('/selfapply/customers', [CustomerController::class, 'users'])->name('selfapply.users');
 //     Route::get('/selfapply/users-details/{userId}', [CustomerController::class, 'usersDetails'])->name('selfapply.customer.details');
 //     Route::post('/selfapply/users-details/update', [CustomerController::class, 'usersDetailsUpdate'])->name('selfapply.customers.update');
-//     Route::get('/selfapply/users-invoice/{userId}/{cardId}', [CustomerController::class, 'generateInvoice'])->name('selfapply.customers.invoice');
+       Route::get('/selfapply/users-invoice/{userId}/{cardId}', [CustomerController::class, 'generateInvoice'])->name('selfapply.customers.invoice');
 //     Route::post('/selfapply/users/update-password', [CustomerController::class, 'updatePassword'])->name('selfapply.customers.update.password');
 //     Route::post('/selfapply/user/assign-agent', [CustomerController::class,'assignAgent'])->name('selfapply.customers.assign.agent');
 //     Route::post('/selfapply/user/deactivate-account',[CustomerController::class,'deactivateAccount'])->name('selfapply.customers.deactivate.account');
@@ -28,4 +28,4 @@ use Modules\Customer\App\Http\Controllers\CustomerController;
 //     Route::post('/selfapply/applications-details/store-remarks', [CustomerController::class, 'storeRemarks'])->name('selfapply.loan.applications.store.remarks');
 //     Route::post('/selfapply/applications-details/delete-remarks', [CustomerController::class, 'deleteRemarks'])->name('selfapply.loan.applications.destroy.remarks');
 //     Route::get('/selfapply/applications-details/download-report/{userid}/{appid}', [CustomerController::class, 'downloadReport'])->name('selfapply.loan.application.download.report');
-// });
+});
